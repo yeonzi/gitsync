7793329341956172830c298dcc70017449b682e3
Lee Yeonji <yeonji@ieee.org>
2020-11-16 14:45:37 +0900

    "[AUTO] git-sync auto commit"

--- gitsync.sh
+++ gitsync.sh
@@ -3,6 +3,11 @@
 CONF_FILE=.gitsync_conf
 
 CMD_STATUS="git status --porcelain"
+CMD_STAGE="git add --all"
+CMD_COMMIT_MSG="echo \"[AUTO] git-sync auto commit\""
+CMD_COMMIT="git commit -m"
+CMD_PUSH="git push --porcelain --force"
+CMD_PULL="git pull -q"
 
 SYNC_TIMEOUT=10
 LOG_FILE=/dev/stdout
@@ -12,32 +17,50 @@ write_log () {
 	echo "${TIME_NOW}: ${1}" >> $LOG_FILE
 }
 
+git_commit_all () {
+	STAGE_STATUS=$($CMD_STAGE)
+
+	COMMIT_MSG=$($CMD_COMMIT_MSG)
+	echo $COMMIT_MSG
+	COMMIT_STATUS=$($CMD_COMMIT "${COMMIT_MSG}")
+}
+
+git_push () {
+	write_log "Pushing to Remote"
+	PUSH_STATUS=$($CMD_PUSH)
+	write_log ${PUSH_STATUS}
+}
+
+git_pull () {
+	write_log "Checking Remote"
+	PULL_STATUS=$($CMD_PULL)
+}
+
 if [[ -e $CONF_FILE ]]; then
 	chmod +x ${CONF_FILE}
 	exec ${CONF_FILE}
 fi
 
-write_log "Started."
+WORK_DIR=$(pwd)
+write_log "Started at ${WORK_DIR}"
 
 while true; do
 
 	GIT_STATUS=$($CMD_STATUS)
 
-	if [[ -z $GIT_STATUS ]]; then
-		write_log "Local repository is clean."
+	if [[ -n $GIT_STATUS ]]; then
+		git_commit_all
+
 	else
-		write_log "Preparing commit."
-		git add --all
-		git commit -m "[AUTO] git-sync auto commit"
+		write_log "Local repository is clean."
 
 	fi
 
-	write_log "Fetching remote"
-	git pull --rebase
+	git_pull
 
 	if [[ -n $GIT_STATUS ]]; then
-		write_log "Push to remote"
-		git push
+		git_push
+
 	fi
 
 	sleep ${SYNC_TIMEOUT}
